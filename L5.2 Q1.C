2e5dfe929232c87c946119d4bb37f4103f8a78e4
Vivekprajapati04 <161919206+Vivekprajapati04@users.noreply.github.com>
2024-03-08 17:51:37 +0530

    Add files via upload

---
+++ L5.2 Q1.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a,b,c;
+	printf("Enter value of A= ");
+	scanf("%d",&a);
+	printf("Enter value of B= ");
+	scanf("%d",&b);
+	printf("Enter value of c= ");
+	scanf("%d",&c);
+	
+	
+	if (a<b)
+	{
+		if (a<c)
+		{
+			printf("A is min");
+		}
+		else
+		{
+			printf("C is min");
+		}	
+	}
+	else
+	{
+		if (b<c)
+		{
+			printf("B is min");
+		}
+		else
+		{
+			printf("C is min");
+		}	
+	}
+}
